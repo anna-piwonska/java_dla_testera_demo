a70b7259f6a14046fd98aa5abccd7dd7d81689a2
Ania <anna@piwonska.pl>
2022-10-24 19:28:52 +0200

    Added another instruction

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -1,5 +1,6 @@
 public class Hello_world {
     public static void main(String[] args) {
         System.out.println("test hello world!!");
+        System.out.println("Another text message...");
     }
 }
