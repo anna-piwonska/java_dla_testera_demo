05fd8ac3806308b8e792404658c44e13ca66d788
Ania <anna@piwonska.pl>
2022-10-24 19:23:12 +0200

    Added Hello_world.java

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -1,5 +1,5 @@
 public class Hello_world {
     public static void main(String[] args) {
-        System.out.println("test hello world!");
+        System.out.println("test hello world!!");
     }
 }
